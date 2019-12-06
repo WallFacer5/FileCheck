201b7f7dde291428cd4b6fe478e029039839bde2
WallFacer5 <zyh9715@126.com>
2019-12-06 11:56:25 -0500

    First result ok

--- checker.py
+++ checker.py
@@ -3,18 +3,24 @@ from md5 import Md5Checker
 
 class Checker:
     def __init__(self, msg, algo):
-        msg = eval('0x' + bytes(msg.encode()).hex())
+        if msg == '':
+            msg = 0
+        else:
+            msg = eval('0x' + bytes(msg.encode()).hex())
         self.byte_stream = msg
+        print('Msg:')
         print(bin(msg))
         print(hex(msg))
-        print(msg.bit_length())
+        print()
         if algo == 'md5':
             self.checker = Md5Checker(self.byte_stream)
 
+    def get_hash(self):
+        return self.checker.get_hash()
+
 
 if __name__ == '__main__':
-    checker = Checker('jasdljf8afj98ajf98jsad9f8j9asdjf9s8djf8asjf98ajs8fjas98fja98jf9as8jf89jfs89jf89jfasjfjsa' +
-                      'sdfasdfasdfsafkashfkhasfkjhjsahfjhfjkshafjahfjshfjahfjhasfjhasfjhasjkfhjkshfjkhfjhffhjdh' +
-                      'sjfkjasfljaskldjfksajfksladjfksjfksjflksjflkjsklfjsakjfklsjfksaljfklsdjfkljaskfljakslfjf' +
-                      'djsflksajfkljsdkfjskdfjksaljfakdjfklsjfklsdjfklsjfakljfklsajfksajfksajfklsajfklajfkjffff'
-                      , 'md5')
+    msg = input('Please input the msg: ')
+    hash_func = input('Please input the hash function you want to use(md5, sha3): ')
+    checker = Checker(msg, hash_func)
+    print(hash_func, 'hash of', msg, 'is', checker.get_hash())
--- md5.py
+++ md5.py
@@ -8,6 +8,7 @@ class Md5Checker:
         self.B = 0xefcdab89
         self.C = 0x98badcfe
         self.D = 0x10325476
+        self.md5_result = None
         self.s = [
             7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
             5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
@@ -18,9 +19,10 @@ class Md5Checker:
         for i in range(64):
             self.K.append(math.floor(2 ** 32 * abs(math.sin(i + 1))))
         self.padding()
+        print('Bytes after padding:')
         print(bin(self.byte_stream))
         print(hex(self.byte_stream))
-        print(self.byte_stream.bit_length())
+        print()
         self.chunk_num = (len(bin(self.byte_stream)[2:]) + 511) // 512
         self.chunks = []
         dup_bytes = self.byte_stream
@@ -28,9 +30,13 @@ class Md5Checker:
             self.chunks.append(dup_bytes % (2 ** 512))
             dup_bytes = dup_bytes >> 512
         self.chunks.reverse()
-        print('\n'.join(list(map(lambda chunk: bin(chunk)[2:], self.chunks))))
+        print('\n'.join(['Chunks:'] + list(map(lambda chunk: bin(chunk)[2:], self.chunks))))
+        print()
 
     def padding(self):
+        if self.byte_stream == 0:
+            self.byte_stream = 0b1<<511
+            return
         origin_length = (len(bin(self.byte_stream)[2:]) + 7) // 8 * 8
         self.byte_stream = self.byte_stream << 8
         self.byte_stream += 0x80
@@ -40,65 +46,157 @@ class Md5Checker:
         self.byte_stream = (self.byte_stream << 64) + origin_length % (2 ** 64)
 
     def F(self, X, Y, Z):
-        return (X & Y) | ((~X) & Z)
+        return (((X & Y) | ((0xffffffff - (X & 0xffffffff)) & Z)) & 0xffffffff)
 
     def G(self, X, Y, Z):
-        return (X & Z) | (Y & (~Z))
+        return (((X & Z) | (Y & (0xffffffff - (Z & 0xffffffff)))) & 0xffffffff)
 
     def H(self, X, Y, Z):
-        return X ^ Y ^ Z
+        return ((X ^ Y ^ Z) & 0xffffffff)
 
     def I(self, X, Y, Z):
-        return Y ^ (X | (~Z))
+        return ((Y ^ (X | (0xffffffff - (Z & 0xffffffff)))) & 0xffffffff)
 
     def FF(self, a, b, c, d, Mj, s, ti):
+        # print('FF:')
+        # print(hex(a), hex(b), hex(c), hex(d), hex(Mj), s, hex(ti))
         a += (self.F(b, c, d) & 0xffffffff) + Mj + ti
         a = ((a & 0xffffffff) << s) | ((a & 0xffffffff) >> (32 - s))
         a += b
+        # print(hex(a), hex(b), hex(c), hex(d))
+        # print(a, b, c, d)
+        # print()
         return a & 0xffffffff
 
     def GG(self, a, b, c, d, Mj, s, ti):
+        # print('GG:')
+        # print(hex(a), hex(b), hex(c), hex(d), hex(Mj), s, hex(ti))
         a += (self.G(b, c, d) & 0xffffffff) + Mj + ti
         a = ((a & 0xffffffff) << s) | ((a & 0xffffffff) >> (32 - s))
         a += b
+        # print(hex(a), hex(b), hex(c), hex(d))
+        # print()
         return a & 0xffffffff
 
     def HH(self, a, b, c, d, Mj, s, ti):
+        # print('HH:')
+        # print(hex(a), hex(b), hex(c), hex(d), hex(Mj), s, hex(ti))
         a += (self.H(b, c, d) & 0xffffffff) + Mj + ti
         a = ((a & 0xffffffff) << s) | ((a & 0xffffffff) >> (32 - s))
         a += b
+        # print(hex(a), hex(b), hex(c), hex(d))
+        # print()
         return a & 0xffffffff
 
     def II(self, a, b, c, d, Mj, s, ti):
+        # print('II:')
+        # print(hex(a), hex(b), hex(c), hex(d), hex(Mj), s, hex(ti))
         a += (self.I(b, c, d) & 0xffffffff) + Mj + ti
         a = ((a & 0xffffffff) << s) | ((a & 0xffffffff) >> (32 - s))
         a += b
+        # print(hex(a), hex(b), hex(c), hex(d))
+        # print()
         return a & 0xffffffff
+
+    def reverse4bytes(self, four_bytes):
+        four_bytes = hex(four_bytes)[2:]
+        four_bytes = '0' * (8 - len(four_bytes)) + four_bytes
+        four_bytes = [four_bytes[:2], four_bytes[2:4], four_bytes[4:6], four_bytes[6:]]
+        four_bytes.reverse()
+        return eval('0x' + ''.join(four_bytes))
 
     def single_chunk_process(self, chunk):
         words = []
         for i in range(16):
-            words.append(chunk & 0xffffffff)
+            words.append(self.reverse4bytes(chunk & 0xffffffff))
             chunk = chunk >> 32
-            words.reverse()
+        words.reverse()
+        tmp = words[-1]
+        words[-1] = words[-2]
+        words[-2] = tmp
+        words[-1] = self.reverse4bytes(words[-1])
+        words[-2] = self.reverse4bytes(words[-2])
+        # print('\n'.join(['words'] + list(map(lambda word: bin(word)[2:], words))))
         a, b, c, d = self.A, self.B, self.C, self.D
+        # print('abcd:', hex(a), hex(b), hex(c), hex(d))
         for i in range(4):
             a = self.FF(a, b, c, d, words[4 * i + 0], self.s[4 * i + 0], self.K[4 * i + 0])
-            b = self.FF(d, a, b, c, words[4 * i + 1], self.s[4 * i + 1], self.K[4 * i + 1])
+            d = self.FF(d, a, b, c, words[4 * i + 1], self.s[4 * i + 1], self.K[4 * i + 1])
             c = self.FF(c, d, a, b, words[4 * i + 2], self.s[4 * i + 2], self.K[4 * i + 2])
-            d = self.FF(b, c, d, a, words[4 * i + 3], self.s[4 * i + 3], self.K[4 * i + 3])
+            b = self.FF(b, c, d, a, words[4 * i + 3], self.s[4 * i + 3], self.K[4 * i + 3])
+        # print(hex(a), hex(b), hex(c), hex(d))
+        words_id = 1
         for i in range(4):
-            a = self.GG(a, b, c, d, words[4 * i + 0], self.s[4 * i + 0], self.K[4 * i + 0])
-            b = self.GG(d, a, b, c, words[4 * i + 1], self.s[4 * i + 1], self.K[4 * i + 1])
-            c = self.GG(c, d, a, b, words[4 * i + 2], self.s[4 * i + 2], self.K[4 * i + 2])
-            d = self.GG(b, c, d, a, words[4 * i + 3], self.s[4 * i + 3], self.K[4 * i + 3])
+            a = self.GG(a, b, c, d, words[words_id], self.s[4 * i + 16], self.K[4 * i + 16])
+            words_id += 5
+            words_id %= 16
+            d = self.GG(d, a, b, c, words[words_id], self.s[4 * i + 17], self.K[4 * i + 17])
+            words_id += 5
+            words_id %= 16
+            c = self.GG(c, d, a, b, words[words_id], self.s[4 * i + 18], self.K[4 * i + 18])
+            words_id += 5
+            words_id %= 16
+            b = self.GG(b, c, d, a, words[words_id], self.s[4 * i + 19], self.K[4 * i + 19])
+            words_id += 5
+            words_id %= 16
+        words_id = 5
         for i in range(4):
-            a = self.HH(a, b, c, d, words[4 * i + 0], self.s[4 * i + 0], self.K[4 * i + 0])
-            b = self.HH(d, a, b, c, words[4 * i + 1], self.s[4 * i + 1], self.K[4 * i + 1])
-            c = self.HH(c, d, a, b, words[4 * i + 2], self.s[4 * i + 2], self.K[4 * i + 2])
-            d = self.HH(b, c, d, a, words[4 * i + 3], self.s[4 * i + 3], self.K[4 * i + 3])
+            a = self.HH(a, b, c, d, words[words_id], self.s[4 * i + 32], self.K[4 * i + 32])
+            words_id += 3
+            words_id %= 16
+            d = self.HH(d, a, b, c, words[words_id], self.s[4 * i + 33], self.K[4 * i + 33])
+            words_id += 3
+            words_id %= 16
+            c = self.HH(c, d, a, b, words[words_id], self.s[4 * i + 34], self.K[4 * i + 34])
+            words_id += 3
+            words_id %= 16
+            b = self.HH(b, c, d, a, words[words_id], self.s[4 * i + 35], self.K[4 * i + 35])
+            words_id += 3
+            words_id %= 16
+        words_id = 0
         for i in range(4):
-            a = self.II(a, b, c, d, words[4 * i + 0], self.s[4 * i + 0], self.K[4 * i + 0])
-            b = self.II(d, a, b, c, words[4 * i + 1], self.s[4 * i + 1], self.K[4 * i + 1])
-            c = self.II(c, d, a, b, words[4 * i + 2], self.s[4 * i + 2], self.K[4 * i + 2])
-            d = self.II(b, c, d, a, words[4 * i + 3], self.s[4 * i + 3], self.K[4 * i + 3])
+            a = self.II(a, b, c, d, words[words_id], self.s[4 * i + 48], self.K[4 * i + 48])
+            words_id += 7
+            words_id %= 16
+            d = self.II(d, a, b, c, words[words_id], self.s[4 * i + 49], self.K[4 * i + 49])
+            words_id += 7
+            words_id %= 16
+            c = self.II(c, d, a, b, words[words_id], self.s[4 * i + 50], self.K[4 * i + 50])
+            words_id += 7
+            words_id %= 16
+            b = self.II(b, c, d, a, words[words_id], self.s[4 * i + 51], self.K[4 * i + 51])
+            words_id += 7
+            words_id %= 16
+
+        # print('abcd:', hex(a), hex(b), hex(c), hex(d))
+        # print('abcd:', bin(a), bin(b), bin(c), bin(d))
+        # print('ABCD:', hex(self.A), hex(self.B), hex(self.C), hex(self.D))
+        # print('ABCD:', bin(self.A), bin(self.B), bin(self.C), bin(self.D))
+        self.A += a
+        self.B += b
+        self.C += c
+        self.D += d
+        self.A &= 0xffffffff
+        self.B &= 0xffffffff
+        self.C &= 0xffffffff
+        self.D &= 0xffffffff
+        print('abcd:', hex(self.A), hex(self.B), hex(self.C), hex(self.D))
+
+    def hashing(self):
+        for chunk in self.chunks:
+            self.single_chunk_process(chunk)
+        self.A = self.reverse4bytes(self.A)
+        self.B = self.reverse4bytes(self.B)
+        self.C = self.reverse4bytes(self.C)
+        self.D = self.reverse4bytes(self.D)
+        self.md5_result = hex(self.A)[2:] + hex(self.B)[2:] + hex(self.C)[2:] + hex(self.D)[2:]
+
+    def get_hash(self):
+        if self.md5_result is None:
+            self.hashing()
+        return self.md5_result
+
+
+if __name__ == '__main__':
+    md5_obj = Md5Checker(0)
+    print(md5_obj.get_hash())
