4cc1535965044f861781fabc039e7c192b5368c2
WallFacer5 <zyh9715@126.com>
2019-12-10 13:58:56 -0500

    md5 and ui_basic ok

--- checker.py
+++ checker.py
@@ -1,22 +1,36 @@
 from md5 import Md5Checker
 from sha3 import Sha3Checker
 import tkinter as tk
+import tkinter.filedialog as fd
+import bitstring
 
 class Checker:
-    def __init__(self, msg, algo):
-        if msg == '':
-            msg = 0
+    def __init__(self, msg, algo, is_file=False):
+        if is_file:
+            msg = bitstring.BitArray(open(msg, 'rb').read()).int
+            self.byte_stream = msg
+            print('Msg:')
+            print(bin(msg))
+            print(hex(msg))
+            print()
+            if algo == 'md5':
+                self.checker = Md5Checker(self.byte_stream)
+            elif algo == 'sha3':
+                self.checker = Sha3Checker(self.byte_stream)
         else:
-            msg = eval('0x' + bytes(msg.encode()).hex())
-        self.byte_stream = msg
-        print('Msg:')
-        print(bin(msg))
-        print(hex(msg))
-        print()
-        if algo == 'md5':
-            self.checker = Md5Checker(self.byte_stream)
-        elif algo == 'sha3':
-            self.checker = Sha3Checker(self.byte_stream)
+            if msg == '':
+                msg = 0
+            else:
+                msg = eval('0x' + bytes(msg.encode()).hex())
+            self.byte_stream = msg
+            print('Msg:')
+            print(bin(msg))
+            print(hex(msg))
+            print()
+            if algo == 'md5':
+                self.checker = Md5Checker(self.byte_stream)
+            elif algo == 'sha3':
+                self.checker = Sha3Checker(self.byte_stream)
 
     def get_hash(self):
         return self.checker.get_hash()
@@ -35,6 +49,7 @@ class Application(tk.Frame):
         self.master = master
         self.pack()
         self.choose_input()
+        self.file_path = tk.StringVar(value='')
 
     def choose_input(self):
         self.radio_buttons = tk.Frame(self)
@@ -100,11 +115,64 @@ class Application(tk.Frame):
         self.hash_out_text.insert(tk.END, checker.get_hash())
         self.hash_out_text.config(state=tk.DISABLED)
 
+    def file_selection(self):
+        file_want = fd.askopenfile()
+        file_path = file_want.name
+        print(file_path)
+        self.file_input.insert(tk.END, file_path)
+        file_want.close()
+        self.file_path.set(file_path)
+        self.file_input.config(text=file_path)
+
     def jump_file(self):
         self.file_frame = tk.Frame(self)
         self.file_input = tk.Entry(self.file_frame)
-        self.file_input.pack()
+        self.file_label = tk.Label(self.file_frame, text='Input file')
+        self.file_input = tk.Entry(self.file_frame, width=58)
+        self.file_choose_btn = tk.Button(self.file_frame, text='Select File', command=self.file_selection)
+
+        self.file_label.grid(row=0, column=0, sticky=tk.W)
+        self.file_input.grid(row=0, column=1, sticky=tk.W)
+        self.file_choose_btn.grid(row=0, column=2, sticky=tk.W)
+
+        self.hash_func = tk.IntVar()
+
+        self.hash_label = tk.Label(self.file_frame, text='Hash Function')
+        self.hash_frame = tk.Frame(self.file_frame)
+        self.md5_rb = tk.Radiobutton(self.hash_frame, variable=self.hash_func, text='md5', value=0)
+        self.sha3_rb = tk.Radiobutton(self.hash_frame, variable=self.hash_func, text='sha3', value=1)
+        self.hash_label.grid(row=1, column=0, sticky=tk.W)
+        self.md5_rb.grid(row=0, column=0, sticky=tk.W)
+        self.sha3_rb.grid(row=0, column=1, sticky=tk.W)
+        self.hash_frame.grid(row=1, column=1, sticky=tk.W)
+
+        self.file_button = tk.Button(self.file_frame, text='Hash!', command=self.do_file_hash)
+        self.file_button.grid(row=2, columnspan=3)
+
+        self.hash_out_label = tk.Label(self.file_frame, text='Hash Result')
+        self.hash_out_text = tk.Text(self.file_frame, height=1, width=87)
+        self.hash_out_text.config(highlightbackground='#80C0FF', state=tk.DISABLED)
+        self.hash_out_label.grid(row=3, column=0)
+        self.hash_out_text.grid(row=3, column=1, columnspan=2)
         self.file_frame.pack()
+
+    def do_file_hash(self):
+        if self.file_path.get() == '':
+            pass
+        else:
+            hash_func = self.hash_func.get()
+            if hash_func == 0:
+                hash_func = 'md5'
+            else:
+                hash_func = 'sha3'
+            print(hash_func)
+            checker = Checker(self.file_path.get(), hash_func, is_file=True)
+            print(hash_func, 'hash of', self.file_path.get(), 'is', checker.get_hash())
+
+            self.hash_out_text.config(state=tk.NORMAL)
+            self.hash_out_text.delete(0.0, tk.END)
+            self.hash_out_text.insert(tk.END, checker.get_hash())
+            self.hash_out_text.config(state=tk.DISABLED)
 
     def say_hi(self):
         print("hi there, everyone!")
